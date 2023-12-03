1d1cd7275ad1c26bbf25db8ab0a29d972a245af4
boby <bobyclaws@gmail.com>
2023-12-04 02:58:15 +0530

    clean up

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 
 insert into ledger_record (id, amount, remarks, created, updated)
 values(1000, 0, 'one hundred', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+insert into TAG(tag_name, frequency)
+values ( 'one', 1.0 ),
+       ('hundred', 1.0);
